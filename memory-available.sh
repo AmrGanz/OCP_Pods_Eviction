c49fd63122ba226746c38b5f6ed872f7ba48c39a
Amr Elganzory <43642001+AmrGanz@users.noreply.github.com>
2021-05-21 16:59:17 +0400

    Check for the available memory
    Simulating the way Kubelet checks for the available memory in a node.
    
    Note:
    This is a modified version of the original script:
    - https://kubernetes.io/examples/admin/resource/memory-available.sh

---
+++ memory-available.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#!/usr/bin/env bash
+
+# This script reproduces what the kubelet does
+# to calculate memory.available relative to root cgroup.
+
+echo "============================================================================================"
+echo "THIS IS A MODIFIED VERSION OF THE ORIGINAL SCRIPT WHICH CAN BE FOUND HERE:"
+echo "https://kubernetes.io/examples/admin/resource/memory-available.sh"
+echo "============================================================================================"
+
+# current memory usage
+memory_capacity_in_kb=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
+memory_capacity_in_bytes=$((memory_capacity_in_kb * 1024))
+memory_capacity_in_Mbytes=`awk "BEGIN {print ($memory_capacity_in_kb)/1024}"`
+memory_capacity_in_Gbytes=`awk "BEGIN {print ($memory_capacity_in_Mbytes)/1024}"`
+memory_usage_in_bytes=$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes)
+memory_usage_in_kb=`awk "BEGIN {print ($memory_usage_in_bytes)/1024}"`
+memory_usage_in_Mbytes=`awk "BEGIN {print ($memory_usage_in_kb)/1024}"`
+memory_usage_in_Gbytes=`awk "BEGIN {print ($memory_usage_in_Mbytes)/1024}"`
+memory_total_inactive_file=$(cat /sys/fs/cgroup/memory/memory.stat | grep total_inactive_file | awk '{print $2}')
+
+memory_working_set=${memory_usage_in_bytes}
+if [ "$memory_working_set" -lt "$memory_total_inactive_file" ];
+then
+    memory_working_set=0
+else
+    memory_working_set=$((memory_usage_in_bytes - memory_total_inactive_file))
+fi
+
+memory_available_in_bytes=$((memory_capacity_in_bytes - memory_working_set))
+memory_available_in_kb=`awk "BEGIN {print ($memory_available_in_bytes)/1024}"`
+memory_available_in_mb=`awk "BEGIN {print ($memory_available_in_kb)/1024}"`
+memory_available_in_gb=`awk "BEGIN {print ($memory_available_in_mb)/1024}"`
+
+
+echo "memory.capacity_in_bytes $memory_capacity_in_bytes"
+echo "memory.usage_in_bytes $memory_usage_in_bytes"
+echo "memory.total_inactive_file $memory_total_inactive_file"
+echo "memory.working_set $memory_working_set"
+echo "memory.available_in_bytes $memory_available_in_bytes"
+echo "memory.available_in_kb $memory_available_in_kb"
+echo "memory.available_in_mb $memory_available_in_mb"
+echo "memory.available_in_gb $memory_available_in_gb"
